6e8703794776db89c652afc21879aea35d8f90cf
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-30 07:28:07 +0500

    4-oy 6-dars vazifasi yuklandi

---
+++ index.js
@@ -0,0 +1,69 @@
+// 1-masala
+
+// for(let i=1; i<=50; i++){
+//     if(i%2==1){
+//         console.log(Math.pow(i, 2));
+//     }
+// }
+
+// 2-masala
+// for(let i=1; i<=10; i++){
+//     console.log(Math.pow(i, 3));
+
+// }
+
+// 3-masala
+// let p=1;
+// for(let i=1; i<=20; i++){
+//     p*=i;
+//     console.log(p);
+// }
+
+// 4-masala
+// let sum=0;
+// for(let i=1; i<=30; i++){
+//     sum+=Math.pow(i, 2);
+// }
+// console.log(sum);
+
+// // 5-masala
+// let n=20
+// let sum=0
+// for(let i=2; i<=n; i++){
+//     for(let k=1; k<=i; k++){
+//         if(i%k==0){
+//             sum++;
+//         }
+//     }
+//     if(sum==2){
+//         console.log(i);
+//         sum=0;
+//     }else{
+//         sum=0;
+//     }
+// }
+
+// 6-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i * 2 < 100) {
+//     console.log(i);
+//   }
+// }
+
+// 7-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 == 0 && i % 5 == 1) {
+//     console.log(i);
+//   }
+// }
+
+
+// 8-masala
+// let n=60;
+// let counter=0;
+// for(let i=11; i<=n; i++){
+//     if(n%i==0){
+//         counter+=i;
+//     }
+// }
+// console.log(counter);
